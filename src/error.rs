9e67069902d080b613ad8658db2837e4f5401c87
Satyam Singh <trueleo@protonmail.com>
2024-06-07 21:51:30 +0530

    Use anyhow

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,13 @@
+use std::borrow::Cow;
+
+use anyhow::anyhow;
+
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("{0}")]
-    TerminationError(Box<dyn std::error::Error + Send + Sync + 'static>),
-    #[error("{0}")]
-    GenericError(#[from] Box<dyn std::error::Error + Send + Sync + 'static>),
+    #[error(transparent)]
+    TerminationError(anyhow::Error),
+    #[error(transparent)]
+    GenericError(#[from] anyhow::Error),
 }
 
 impl Error {
@@ -13,7 +17,11 @@ impl Error {
 }
 
 impl Error {
-    pub fn new_generic(err: &str) -> Self {
-        Self::GenericError(err.into())
+    pub fn new(err: impl Into<Cow<'static, str>>) -> Self {
+        Self::GenericError(anyhow!(err.into()))
+    }
+
+    pub fn termination(err: impl Into<Cow<'static, str>>) -> Self {
+        Self::TerminationError(anyhow!(err.into()))
     }
 }
