dae9daa40517e462e9916005f109454cab4258a0
Satyam Singh <trueleo@protonmail.com>
2024-06-30 18:55:38 +0530

    Add crate level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,123 @@
+/*! Rusher is an simple and easy load testing framework for Rust,
+inspired by [grafana k6](https://k6.io) and powered by
+[`tracing`](https://docs.rs/tracing/latest/tracing/) crate.
+
+Rusher is agnostic over what a user is suppose to do. Metrics for a test is generated via use of tracing span and events inside of a user call, more about this is documented below.
+These spans and events are collected via a tracing subscriber. For common usecases this crate provides wrapper types over popular async clients which emit proper tracing spans and events.
+
+# Concepts
+
+Load testing is a process where an application is subjected to a specific
+amount of simulated load to evaluate its performance and reliability.
+By simulating multiple users accessing the application simultaneously, developers can identify potential bottlenecks, optimize performance, and
+ensure the system can handle expected traffic.
+
+* *Virtual Users* - Implementaion of User trait defines how to simulate this interaction with your application in ways similar to a real users. For more detail explaination look at [`User`](user::User).
+Users of this framework are suppose to implement [`User`](user::User) trait for each variant of user along with their user builder function.
+
+* *User Builder* - A user builder function defines how to create a User instance. A User can borrow shared data from a [RuntimeDataStore](data::RuntimeDataStore).
+
+* *Datastores* - Each execution creates a [RuntimeDataStore](data::RuntimeDataStore) before it begins execution, which is used to hold shared data that will be used by User.
+
+* *Executors* - Executors control how rusher will allocate Users and perform iterations. Look at Executor docs for more details.
+
+* *Scenario* - Scenario is grouping of one or more executors. Scenarios are ran sequentially in order that they are defined.
+
+
+# Example
+```no_run
+use std::time::Duration;
+
+use rusher::client::reqwest::Client;
+use rusher::error::Error;
+use rusher::prelude::*;
+
+struct MyUser<Iter> {
+    client: Client,
+    post_content: Iter,
+}
+
+#[async_trait::async_trait]
+impl<'a, Iter> User for MyUser<Iter>
+where
+    Iter: Iterator<Item = &'a String> + Send,
+{
+    async fn call(&mut self) -> UserResult {
+        // In each iteration get the next string
+        let body = self.post_content.next().unwrap().to_string();
+        let res = self
+            .client
+            .post("https://httpbin.org/anything")
+            .body(body)
+            .send()
+            .await
+            .map_err(|err| Error::GenericError(err.into()))?;
+
+        if !res.status().is_success() {
+            let body = res
+                .bytes()
+                .await
+                .map_err(|err| Error::TerminationError(err.into()))?;
+
+            let err = String::from_utf8_lossy(&body).to_string();
+            return Err(Error::termination(err));
+        }
+
+        tokio::time::sleep(Duration::from_millis(500)).await;
+
+        Ok(())
+    }
+}
+
+#[apply(rusher::boxed_future)]
+async fn datastore(store: &mut RuntimeDataStore) {
+    let data = vec!["a".to_string(), "b".to_string(), "c".to_string()];
+    store.insert(data);
+    store.insert(Client::new());
+}
+
+async fn user_builder(runtime: &RuntimeDataStore) -> impl User + '_ {
+    let client: &Client = runtime.get().unwrap();
+    let content: &Vec<String> = runtime.get().unwrap();
+
+    MyUser {
+        client: client.clone(),
+        post_content: content.iter().cycle(),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let execution_ramping_user = ExecutionPlan::builder()
+        .with_user_builder(user_builder)
+        .with_data(datastore)
+        .with_executor(Executor::RampingUser {
+            pre_allocate_users: 10,
+            stages: vec![(1, Duration::from_secs(10)), (1, Duration::from_secs(3))],
+        });
+
+    let scenario1 = Scenario::new("scene1".to_string(), execution_ramping_user);
+
+    let execution_once = ExecutionPlan::builder()
+        .with_user_builder(user_builder)
+        .with_data(datastore)
+        .with_executor(Executor::Once);
+
+    let scenario2 = Scenario::new("scene2".to_string(), execution_once);
+
+    let scenarios = vec![scenario1, scenario2];
+
+    Runner::new(scenarios).enable_web(true).run().await.unwrap();
+}
+```
+
+### Features
+- `tui` Enables tui mode, allowing for user to look at live feed of execution in terminal.
+- `web` Enables web mode which contains a simple axum server along with a inbuilt UI for looking at updates.
+- `serde` - Enable serialization with serde.
+- `reqwest` - Wrapper client type for reqwest.
+*/
+
 #[cfg(any(feature = "tui", feature = "web"))]
 pub mod app;
 
